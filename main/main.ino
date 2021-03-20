72ed1ca161120e6ee1b1971a1f322f2409ce9bbb
Chris Hoverd <christopher.hoverd@ontariotechu.net>
2021-03-20 18:39:42 -0400

    added speaker alarm system

--- main/main.ino
+++ main/main.ino
@@ -4,16 +4,18 @@
 
 #define IR A0
 #define model 1080
-int LEDPIN = 6;
-int NUMPIXELS = 8;
+int LED_PIN = 6;
+int NUM_PIXELS = 8;
+int SPEAKER_PIN = 7;
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 SharpIR SharpIR(IR, model);
 int distance;
 int delayval = 500; // delay for half a second
 
 void setup() {
   pinMode(IR, INPUT);
+  pinMode(SPEAKER_PIN, INPUT);
   pixels.begin(); // This initializes the NeoPixel library.
 
   Serial.begin(9600);
@@ -38,6 +40,7 @@ void loop() {
       pixels.setPixelColor(5, pixels.Color(0,255,0)); 
       pixels.setPixelColor(6, pixels.Color(0,255,0)); 
       pixels.setPixelColor(7, pixels.Color(0,255,0)); 
+      noTone(SPEAKER_PIN);
    }
   else
    {
@@ -49,6 +52,8 @@ void loop() {
       pixels.setPixelColor(5, pixels.Color(255,0,0)); 
       pixels.setPixelColor(6, pixels.Color(255,0,0)); 
       pixels.setPixelColor(7, pixels.Color(255,0,0));
+      digitalWrite(SPEAKER_PIN, HIGH);
+      tone(SPEAKER_PIN, 200);
   }
 
   pixels.show(); // This sends the updated pixel color to the hardware.
