832c6095161956cdc4c7ef044de107c7615dd056
Chris Hoverd <christopher.hoverd@ontariotechu.net>
2021-03-20 21:52:29 -0400

    green LED changes intensity depending on ambient light

--- scenario1-intruder_alarm/scenario1-intruder_alarm.ino
+++ scenario1-intruder_alarm/scenario1-intruder_alarm.ino
@@ -1,3 +1,8 @@
+//cite for photocell(csd sensor) - https://learn.adafruit.com/photocells/arduino-code
+//cite for neopixel stick - https://learn.adafruit.com/adafruit-neopixel-uberguide/arduino-library-use
+//cite for sharp ir sensor - https://github.com/guillaume-rico/SharpIR
+
+
 #include <SharpIR.h>
 #include <Adafruit_NeoPixel.h>
 
@@ -8,6 +13,8 @@
 int LED_PIN = 6;
 int NUM_PIXELS = 8;
 int SPEAKER_PIN = 7;
+int LED_BRIGHTNESS;
+int GREEN_LED = 10;
 
 int distance;
 int photocell_reading;
@@ -20,6 +27,7 @@ void setup() {
   pinMode(IR, INPUT);
   pinMode(SPEAKER_PIN, INPUT);
   pinMode(PHOTOCELL, INPUT);
+  pinMode(GREEN_LED, OUTPUT);
   pixels.begin(); // This initializes the NeoPixel library.
 
   Serial.begin(9600);
@@ -59,21 +67,25 @@ void loop() {
       digitalWrite(SPEAKER_PIN, HIGH);
       tone(SPEAKER_PIN, 200);
   }
-  Serial.print("Analog reading = ");
-  Serial.print(photocell_reading);     // the raw analog reading
- 
-  if (photocell_reading < 10) {
-    Serial.println(" - Dark");
-  } else if (photocell_reading < 200) {
-    Serial.println(" - Dim");
-  } else if (photocell_reading < 500) {
-    Serial.println(" - Light");
-  } else if (photocell_reading < 800) {
-    Serial.println(" - Bright");
-  } else {
-    Serial.println(" - Very bright");
-  }
   pixels.show(); // This sends the updated pixel color to the hardware.
+  Serial.print("Analog reading = ");
+  Serial.println(photocell_reading);     // the raw analog reading
+  LED_BRIGHTNESS = map(1023 - photocell_reading, 0, 1023, 0, 255);
+  Serial.println(LED_BRIGHTNESS);
+  analogWrite(GREEN_LED, LED_BRIGHTNESS );
+//  if(LED_BRIGHTNESS<=50)
+//  {
+//    analogWrite(GREEN_LED, 50);
+//  }
+//
+//  if(50<LED_BRIGHTNESS<=150)
+//  {
+//    analogWrite(GREEN_LED, 150);
+//  }
+//  if(150<LED_BRIGHTNESS<=255)
+//  {
+//    analogWrite(GREEN_LED, 255);
+//  }
 
   delay(delayval); // Delay for a period of time (in milliseconds).
 }
